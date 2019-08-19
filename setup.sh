2adb020311bc36f900361bd6c64497fd0aeab037
takenoko-str <taku.hyogo.kei@gmail.com>
2019-08-19 15:13:01 +0900

    feat: ワークスペースの設置

--- setup.sh
+++ setup.sh
@@ -8,4 +8,7 @@ echo "source $(catkin locate --shell-verbs)" >> ~/.bashrc
 source ~/.bashrc
 
 #roslaunch rosbridge_server rosbridge_websocket.launch
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws
+catkin_make
 
