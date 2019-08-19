333b3cfdf873e874c5240a2a01c3e3c67f23a06c
takenoko-str <taku.hyogo.kei@gmail.com>
2019-08-20 00:26:20 +0900

    fix: packer起動コマンド修正

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ "$1" = "16" ]; then
-  AMI_DISTRO=xenial-16.04 ROS_DISTRO=kinetic packer build packer.json
+  AMI_DISTRO=xenial-16.04 ROS_DISTRO=kinetic packer build -machine-readable packer.json | tee "$2"
 fi
 
 if [ "$1" = "18" ]; then
-  AMI_DISTRO=bionic-18.04 ROS_DISTRO=melodic packer build packer.json
+  AMI_DISTRO=bionic-18.04 ROS_DISTRO=melodic packer build -machine-readable packer.json | tee "$2"
 fi
 
